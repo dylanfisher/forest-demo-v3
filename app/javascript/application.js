b6f0de020c65f72aba181edc7ac769d84c455980
dylanfisher <hi@dylanfisher.com>
2023-05-30 23:04:16 -0700

    Move javascript location

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+
+// Vendor
+import 'lazysizes';
+import '@hotwired/turbo-rails';
+
+// Application
+import './main';
+import './partials/lazy_load_images';
+import './partials/lazy_load_content';
+import './partials/lazy_load_videos';
